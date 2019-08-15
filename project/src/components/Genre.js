e4e34a4bba034d6d2aeb28b484780e4564c23624
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-08-15 15:54:50 -0700

    added PlayButton to Genre component

--- project/src/components/Genre.js
+++ project/src/components/Genre.js
@@ -1,9 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeadphonesAlt } from '@fortawesome/free-solid-svg-icons'
 
-const GenreContainer = styled.div``;
+const GenreContainer = styled.div`
+    display: flex;
+`;
 
-const GenreTitle = styled.h2`
+const GenreTitle = styled.div`
   color: #f4e676;
   font-size: 1.7rem;
   text-align: left;
@@ -13,12 +17,37 @@ const GenreTitle = styled.h2`
   text-transform: uppercase;
   letter-spacing: 2px;
   width: 80%;
+  display: flex;
+  justify-content: space-between;
 `;
+
+
+const PlayButton = styled.button`
+    vertical-align: middle;
+    background: none;
+    color: lightgray;
+    border: 2px solid lightgray;
+    border-radius: 5px;
+    padding: 0.4rem;
+    margin-right: 2.5rem;
+
+    :hover {
+        cursor: pointer;
+        color: white;
+        border-color: white;
+    }
+
+    svg {
+        font-size: 1.4rem;
+    }
+`
+
 
 const Genre = props => {
   return (
     <GenreContainer>
-      <GenreTitle>{props.genre}</GenreTitle>
+      <GenreTitle>{props.genre}<PlayButton><FontAwesomeIcon icon={faHeadphonesAlt} /></PlayButton></GenreTitle>
+      
     </GenreContainer>
   );
 };
